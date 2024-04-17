ac231ef726546e60ff1f06cf82e552cbb01f8911
Dunia6 <jephtedunia07@gmail.com>
2024-04-17 15:40:40 +0300

    Add static

--- sppinapp/settings.py
+++ sppinapp/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = [BASE_DIR / 'static',]
-
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 media_root = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
--- sppinapp/urls.py
+++ sppinapp/urls.py
@@ -58,3 +58,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

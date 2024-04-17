09108eb31c144e9888c5e525366c6e1ce8330c2f
Dunia6 <jephtedunia07@gmail.com>
2024-04-17 15:35:47 +0300

    Add static

--- sppinapp/settings.py
+++ sppinapp/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = [BASE_DIR / 'static',]
+
+STATICFILES_DIRS = [STATIC_DIR]
+
+media_root = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
